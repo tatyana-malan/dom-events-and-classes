c3de766291457d3cd66542ca7959dbf663281f13
Tatyana Malan <tatyana.malan@gmail.com>
2017-12-14 16:29:10 +1300

    added makeBlue

--- game.js
+++ game.js
@@ -11,7 +11,8 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue)
   }
 }
 
@@ -22,7 +23,10 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue (evt) 
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
